739715173cadeb42dc9780bafdfb70c92380b81b
pc <pc@Mava>
2022-10-03 15:11:00 +0100

    first commit

---
+++ Workshop1/src/Workshop.java
@@ -0,0 +1,20 @@
+
+public class Workshop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello world !!");
+		
+		Personne unePersonne = new Personne();
+		unePersonne.setNom("Kouoms");
+		unePersonne.setPrenom("Mayou");
+		System.out.println(unePersonne.getNom() + "\n" + unePersonne.getPrenom());
+		
+		//unePersonne.direBonjour();
+		
+		//unePersonne.direBonsoir();
+		
+	
+	}
+
+}
